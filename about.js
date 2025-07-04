e59cfc42de102dd52e95dcdf25d52451fe46eab8
irdnanig <ginandri283@gmail.com>
2025-07-04 15:08:33 +0700

    about timeline

--- about.js
+++ about.js
@@ -155,3 +155,95 @@ document.fonts.ready.then(() => {
   });
   tl.from(".icons-goal", { autoAlpha: 0 }, "<");
 });
+
+gsap.set(".name_text-one", { autoAlpha: 0 });
+gsap.set(".name_text-two", { autoAlpha: 0 });
+gsap.set(".about-name_right", { yPercent: 50 });
+gsap.set(".about-name_desc", { color: "#DFDDD7" });
+const nameTl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".section_about-name",
+    start: "top top",
+    end: "bottom+=100% top",
+    scrub: true,
+    pin: true,
+    markers: true,
+  },
+});
+nameTl
+  .to(".about-name_right", { yPercent: 18, duration: 1, ease: "none" })
+  .to(
+    ".name_text-one",
+    { autoAlpha: 1, duration: 1, delay: 0.5, ease: "none" },
+    "<"
+  )
+  .to(
+    ".about-name_desc:nth-child(1)",
+    { color: "var(--color--text)", duration: 0.5, ease: "none" },
+    "<"
+  )
+  .to("#maskRectLeft", {
+    attr: { y: -300 },
+    duration: 1.5,
+    ease: "none",
+  })
+  .to(
+    "#maskRectRight",
+    {
+      attr: { y: -300 },
+      duration: 1.5,
+      ease: "none",
+    },
+    "<"
+  )
+  .to(".mask_one", { top: "50%", ease: "none", duration: 1.1 }, "<")
+  .to(".about-name_right", { yPercent: -8, duration: 1, ease: "none" }, "<")
+  .to(
+    ".about-name_desc:nth-child(1)",
+    { autoAlpha: 0, duration: 0.5, delay: 0.5 },
+    "<"
+  )
+  .to(
+    ".about-name_desc:nth-child(2)",
+    { color: "var(--color--text)", duration: 0.5, ease: "none" },
+    "<"
+  )
+  .set("#maskRectLeft", { attr: { y: 300 } })
+  .set("#maskRectRight", { attr: { y: 300 } })
+  .to(".about-name_right", { yPercent: -38, duration: 1, ease: "none" })
+  .to(
+    "#maskRectLeft",
+    {
+      attr: { y: 0 },
+      duration: 1.5,
+      ease: "none",
+    },
+    "<"
+  )
+  .to(
+    ".about-name_desc:nth-child(2)",
+    { autoAlpha: 0, duration: 0.5, delay: 0.5 },
+    "<"
+  )
+  .to(
+    ".about-name_desc:nth-child(3)",
+    { color: "var(--color--text)", duration: 0.5, ease: "none" },
+    "<"
+  )
+  .to("#maskRectRight", {
+    attr: { y: 0 },
+    duration: 1.5,
+    ease: "none",
+  })
+  .to(".about-name_right", { yPercent: -68, duration: 1, ease: "none" }, "<")
+  .to(".mask_one", { top: "0%", ease: "none", duration: 1, delay: 0.5 }, "<")
+  .to(
+    ".about-name_desc:nth-child(3)",
+    { autoAlpha: 0, duration: 0.5, delay: 0.5 },
+    "<"
+  )
+  .to(
+    ".about-name_desc:nth-child(4)",
+    { color: "var(--color--text)", duration: 0.5, ease: "none" },
+    "<"
+  );
